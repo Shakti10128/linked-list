7d7b7673e06784f065df8aa0623db0ffad7c00fa
shakti <kumarshakti2017@gmail.com>
2022-10-11 22:55:34 +0530

    nothing change

--- SortLL_0_1_2.cc
+++ SortLL_0_1_2.cc
@@ -33,6 +33,8 @@ void print(Node * head){
 
 
 // sorting linked list 0_1_2
+/* In this first we will count the occurance of zero, ones, and two 
+ and then we will change the data of the linked list by using while loop*/
 Node * sort_0_1_2(Node * head){
     if(head == NULL){
         return head;
@@ -73,10 +75,70 @@ Node * sort_0_1_2(Node * head){
 
 }
 
+/*Suppose interviwer say to you, you are not allow to change the data of linked list.
+So follow this approach in this you can make different different linked and merge them
+in sorted order*/
+
+void insertAtTail(Node* & tail,Node * curr){
+    tail->next = curr;
+    tail = curr;
+}
+
+Node * SortLL_0_1_2(Node * head){
+    if(head == NULL){
+        return head;
+    }
+    Node * zeroHead = new Node(-1);
+    Node * zeroTail = zeroHead;
+    Node * oneHead = new Node(-1);
+    Node * oneTail = oneHead;
+    Node * twoHead = new Node(-1);
+    Node * twoTail = twoHead;
+
+    Node * curr = head;
+    // creating seperate linked list for 0,1,2
+    while(curr!=NULL){
+        int value = curr->data;
+        if(value == 0){
+            insertAtTail(zeroTail,curr);
+        }
+        if(value ==1){
+            insertAtTail(oneTail,curr);
+        }
+        if(value == 2){
+            insertAtTail(twoTail,curr);
+        }
+        curr = curr->next;
+    }
+    // merge 3 sublist
+    if(oneHead->next!=NULL){
+        zeroTail->next = oneHead->next;
+    }
+    else{
+        zeroTail->next = twoHead->next;
+    }
+    oneTail->next = twoHead->next; 
+    twoTail->next = NULL;
+
+    head = zeroHead->next;
+    // deleting dummy nodes
+    delete zeroHead;
+    delete oneHead;
+    delete twoHead;
+
+    return head;
+} 
+
 //  2 1 0 2 0 1 -1
 int main(){
     Node* head = takeInput();
     Node * temp = head;
-    Node * ans = sort_0_1_2(temp);
-    print(ans);
+    //  by changing the data we are sorting the linked list
+    // Node * ans = sort_0_1_2(temp);
+    // print(ans);
+
+    // making seperate seperate linked list for diff diff value and merging them into one
+    // temp = head;
+    Node * ans1 = SortLL_0_1_2(temp);
+    print(ans1);
 }
